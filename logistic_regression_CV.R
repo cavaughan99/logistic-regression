352debf1bd5f39964d40ee4a1f86d4e54bf26256
cavaughan99 <cavaughan99@gmail.com>
2017-09-17 22:22:57 -0700

    Update logistic_regression_CV.R

--- logistic_regression_CV.R
+++ logistic_regression_CV.R
@@ -1,26 +1,8 @@
-## Regression with binary outcomes
+
 ## ═════════════════════════════════
 
 ## Logistic regression
 ## ───────────────────────
-
-##   This far we have used the `lm' function to fit our regression models.
-##   `lm' is great, but limited–in particular it only fits models for
-##   continuous dependent variables. For categorical dependent variables we
-##   can use the `glm()' function.
-
-##   For these models we will use a different dataset, drawn from the
-##   National Health Interview Survey. From the [CDC website]:
-
-##         The National Health Interview Survey (NHIS) has monitored
-##         the health of the nation since 1957. NHIS data on a broad
-##         range of health topics are collected through personal
-##         household interviews. For over 50 years, the U.S. Census
-##         Bureau has been the data collection agent for the National
-##         Health Interview Survey. Survey results have been
-##         instrumental in providing data to track health status,
-##         health care access, and progress toward achieving national
-##         health objectives.
 
 ##   Load the National Health Interview Survey data:
 
@@ -35,7 +17,7 @@ labs <- attributes(NH11)$labels
 ## Logistic regression example
 ## ───────────────────────────────
 
-##   Let's predict the probability of being diagnosed with hypertension
+##   predict the probability of being diagnosed with hypertension
 ##   based on age, sex, sleep, and bmi
 
 str(NH11$hypev) # check stucture of hypev
@@ -50,13 +32,7 @@ coef(summary(hyp.out))
 ## Logistic regression coefficients
 ## ────────────────────────────────────
 
-##   Generalized linear models use link functions, so raw coefficients are
-##   difficult to interpret. For example, the age coefficient of .06 in the
-##   previous model tells us that for every one unit increase in age, the
-##   log odds of hypertension diagnosis increases by 0.06. Since most of us
-##   are not used to thinking in log odds this is not too helpful!
-
-##   One solution is to transform the coefficients to make them easier to
+##   transform the coefficients to make them easier to
 ##   interpret
 
 hyp.out.tab <- coef(summary(hyp.out))
@@ -65,12 +41,6 @@ hyp.out.tab
 
 ## Generating predicted values
 ## ───────────────────────────────
-
-##   In addition to transforming the log-odds produced by `glm' to odds, we
-##   can use the `predict()' function to make direct statements about the
-##   predictors in our model. For example, we can ask "How much more likely
-##   is a 63 year old female to have hypertension compared to a 33 year old
-##   female?".
 
 # Create a dataset with predictors set at desired levels
 predDat <- with(NH11,
@@ -83,23 +53,16 @@ cbind(predDat, predict(hyp.out, type = "response",
                        se.fit = TRUE, interval="confidence",
                        newdata = predDat))
 
-##   This tells us that a 33 year old female has a 13% probability of
-##   having been diagnosed with hypertension, while and 63 year old female
-##   has a 48% probability of having been diagnosed.
-
 ## Packages for  computing and graphing predicted values
 ## ─────────────────────────────────────────────────────────
-
-##   Instead of doing all this ourselves, we can use the effects package to
-##   compute quantities of interest for us (cf. the Zelig package).
+## use the effects package to
+##   compute quantities of interest (cf. the Zelig package).
 install.packages("effects")
 library(effects)
 plot(allEffects(hyp.out))
 
 ## Exercise: logistic regression
 ## ───────────────────────────────────
-
-##   Use the NH11 data set that we loaded earlier.
 
 ##   1. Use glm to conduct a logistic regression to predict ever worked
 ##      (everwrk) using age (age_p) and marital status (r_maritl).
@@ -141,7 +104,7 @@ r_maritl1 <- factor(r_maritl, levels=c("1 Married - spouse in household","2 Marr
 str(r_maritl1)
 levels(r_maritl1)
 
-# run our regression model predicting everwrk from marital status and age
+# run regression model predicting everwrk from marital status and age
 work.out <- glm(everwrk3~age_p+r_maritl1,
                data=NH11, family="binomial")
 coef(summary(work.out))
@@ -164,10 +127,6 @@ work.out.tab
 
 ##   2. Predict the probability of working for each level of marital
 ##      status.
-
-##   Note that the data is not perfectly clean and ready to be modeled. You
-##   will need to clean up at least some of the variables before fitting
-##   the model.
 
 
 levels(r_maritl1)
